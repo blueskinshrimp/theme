7e04fb18128f6d5c1026c97ae2a74b871c0b6880
十一 <sf996@sina.com>
2017-11-14 20:59:18 +0800

    Update AdminLteAsset.php

--- web/AdminLteAsset.php
+++ web/AdminLteAsset.php
@@ -1,49 +1,23 @@
 <?php
-namespace dmstr\web;
+namespace papayaer\web;
 
 use yii\base\Exception;
-use yii\web\AssetBundle as BaseAdminLteAsset;
+use yii\web\AssetBundle;
 
 /**
- * AdminLte AssetBundle
- * @since 0.1
  */
-class AdminLteAsset extends BaseAdminLteAsset
+class PapayaerAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/almasaeed2010/adminlte/dist';
+    public $sourcePath = '@vendor/creativetimofficial/now-ui-kit/assets';
     public $css = [
-        'css/AdminLTE.min.css',
+        'css/now-ui-kit.css',
     ];
     public $js = [
-        'js/adminlte.min.js'
+        'js/now-ui-kit.js'
     ];
     public $depends = [
-        'rmrevin\yii\fontawesome\AssetBundle',
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
         'yii\bootstrap\BootstrapPluginAsset',
     ];
-
-    /**
-     * @var string|bool Choose skin color, eg. `'skin-blue'` or set `false` to disable skin loading
-     * @see https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#layout
-     */
-    public $skin = '_all-skins';
-
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        // Append skin color file if specified
-        if ($this->skin) {
-            if (('_all-skins' !== $this->skin) && (strpos($this->skin, 'skin-') !== 0)) {
-                throw new Exception('Invalid skin specified');
-            }
-
-            $this->css[] = sprintf('css/skins/%s.min.css', $this->skin);
-        }
-
-        parent::init();
-    }
 }
